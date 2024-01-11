85c412a7ecb8ae7b080ff76973ccbeecfe8717ac
Arber Shkurti <arbershkurti@yahoo.com>
2024-01-11 22:50:41 +0100

    collection

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import HeroSections from './Components/HeroSections';
+import { Container } from 'react-bootstrap';
+import Header from './Components/Header';
+import MenuOverlay from './Components/MenuOverlay';
+import ImagesCollections from './Components/ImagesCollections';
+import { useState } from 'react';
 
 function App() {
+  const[navbarOpen, setNavbarOpen] = useState(false);
+  
   return (
     <div className="App">
-      <header className="App-header">
+         <Container>
+          <Header navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen}/>
+          <MenuOverlay navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen}/>
+          <HeroSections />
+          <ImagesCollections />
+         </Container>
         <div>
-        
-        <img src='./images/image-1.jpg' width={300}/>
-        <img src='./images/image-2.jpg' width={300}/>
-        <img src='./images/image-3.jpg' width={300}/>
-        <img src='./images/image-4.jpg' width={300}/>
+        <img src='./images/image-1.jpg' width={200} margin-bottom={50}/>
+        <img src='./images/image-2.jpg' width={200}/>
+        <img src='./images/image-3.jpg' width={200}/>
+        <img src='./images/image-4.jpg' width={200}/>
         </div>
-      </header>
+
+     
     </div>
+
   );
 }
 
