7e6db6c7f121df1a8344f0beb7455ad17ff65a54
Arber Shkurti <arbershkurti@yahoo.com>
2024-01-11 19:07:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        
+        <img src='./images/image-1.jpg' width={300}/>
+        <img src='./images/image-2.jpg' width={300}/>
+        <img src='./images/image-3.jpg' width={300}/>
+        <img src='./images/image-4.jpg' width={300}/>
+        </div>
       </header>
     </div>
   );
